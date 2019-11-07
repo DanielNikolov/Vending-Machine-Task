9b4ab534b4b7f71bf4f713b6404b63555e6bb896
DanielNikolov <dnikolov@gmail.com>
2019-11-07 14:53:00 +0200

    Improvements

--- vendingMachine.js
+++ vendingMachine.js
@@ -20,13 +20,21 @@ class VendingMachine {
         console.log(`Change = ${arrChange.join(',')}`);
     }
 
+    getNominalMappingArrayAsc() {
+        return Object.entries(this._nominalMapping).sort((a, b) => {
+            if (a[1] < b[1]) {
+                return -1
+            } else if (a[1] > b[1]) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+
     /* Calculates total amount */
     calculateTotalAmount(vault) {
         return Object.keys(vault).reduce((total, key) => {
-            let nominalValue = this._nominalMapping.find(element => {
-                return element.code === key;
-            });
-            return total +  (vault[key] * nominalValue.value / 100);
+            return total +  (vault[key] * this._nominalMapping[key] / 100);
         }, 0);
     }
 
@@ -54,19 +62,18 @@ class VendingMachine {
     }
 
     /* Builds array of all coins for the change or empty array */
-    calculateChange(diff) {
+    calculateChange(diff, sortedNominalMapping) {
         let change = [];
         let totalDiffValue = diff;
-        this._nominalMapping.reverse().forEach(nominalObj => {
-            if (totalDiffValue >= nominalObj.value) {
-                let coinsCount = Math.floor(totalDiffValue / nominalObj.value);
-                let userCoinCount = this._paidMoney[nominalObj.code] ? this._paidMoney[nominalObj.code] : 0;
-                let aggregatedCoinCount = this._moneyStore[nominalObj.code] + userCoinCount;
+        sortedNominalMapping.reverse().forEach(nominalObj => {
+            if (totalDiffValue >= nominalObj[1]) {
+                let coinsCount = Math.floor(totalDiffValue / nominalObj[1]);
+                let userCoinCount = this._paidMoney[nominalObj[0]] ? this._paidMoney[nominalObj[0]] : 0;
+                let aggregatedCoinCount = this._moneyStore[nominalObj[0]] + userCoinCount;
                 coinsCount = Math.min(coinsCount, aggregatedCoinCount);
                 if (coinsCount > 0) {
-                    totalDiffValue = totalDiffValue - (coinsCount * nominalObj.value);
-                    let strCoinIds = (nominalObj.code + ',').repeat(coinsCount);
-                    change = change.concat(strCoinIds.substring(0, strCoinIds.length-1).split(','));
+                    totalDiffValue = totalDiffValue - (coinsCount * nominalObj[1]);
+                    change = change.concat(Array(coinsCount).fill(nominalObj[0]));
                 }
             }
         });
@@ -98,9 +105,10 @@ class VendingMachine {
     processOrder(slotId, product) {
         let changeObj = [];
         let diff = (this.calculateTotalAmount(this._paidMoney) - product.price) * 100;
+        let sortedNominalMapping = this.getNominalMappingArrayAsc();
         /* Cannot return change less than 5c */
-        if (diff >= this._nominalMapping[0].value) {
-            changeObj = this.calculateChange(diff);
+        if (diff >= sortedNominalMapping[0][1]) {
+            changeObj = this.calculateChange(diff, sortedNominalMapping);
         }
         if (changeObj.length > 0) {
             this.updateMoneyInventory(changeObj);
