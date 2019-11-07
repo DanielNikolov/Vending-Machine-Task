99263449ffcc2fae4c859b7a24f14c3bc78cca13
DanielNikolov <dnikolov@gmail.com>
2019-11-07 11:02:04 +0200

    Updated code - removed not needed processing

--- vendingMachine.js
+++ vendingMachine.js
@@ -10,29 +10,22 @@ function askForSelection() {
 class VendingMachine {
 
     constructor(vendingProducts, vendingMoney, userPaid, nominalValues) {
-        this.productsStore = vendingProducts;
-        this.moneyStore = vendingMoney;
-        this.paidMoney = userPaid;
-        this.nominalMapping = nominalValues;
+        this._productsStore = vendingProducts;
+        this._moneyStore = vendingMoney;
+        this._paidMoney = userPaid;
+        this._nominalMapping = nominalValues;
     }
 
     generateChangeMsg(arrChange) {
-        let coinIds = [];
-        arrChange.forEach(element => {
-            for (let i = 0; i < element.qty; i++) {
-                coinIds.push(element.code);
-            }
-        });
-
-        console.log(`Change = ${coinIds.join(',')}`);
+        console.log(`Change = ${arrChange.join(',')}`);
     }
 
     /* Calculate all the money given by user */
     updateTotalPaidAmound(moneyId) {
-        this.paidMoney[moneyId] += 1;
-        this.paidMoney.total = Object.keys(this.paidMoney).reduce((total, key) => {
-            if (this.moneyStore[key] != null && this.moneyStore[key].hasOwnProperty('value')) {
-                return total +  (this.paidMoney[key] * this.moneyStore[key].value);
+        this._paidMoney[moneyId] += 1;
+        this._paidMoney.total = Object.keys(this._paidMoney).reduce((total, key) => {
+            if (this._moneyStore[key] != null && this._moneyStore[key].hasOwnProperty('value')) {
+                return total +  (this._paidMoney[key] * this._moneyStore[key].value);
             } else {
                 return total;
             }
@@ -41,42 +34,32 @@ class VendingMachine {
 
     /* Resets user payment, i.e. does not take money from user */
     resetUserPayment() {
-        Object.keys(this.paidMoney).forEach(key => {
-            this.paidMoney[key] = 0;
+        Object.keys(this._paidMoney).forEach(key => {
+            this._paidMoney[key] = 0;
         });
     }
 
     /* Displays debug information about products and money availability */
     getInventory() {
         console.log('Cache Inventory: ');
-        console.log(this.moneyStore);
+        console.log(this._moneyStore);
         console.log('Products Inventory: ');
-        console.log(this.productsStore);
-    }
-
-    /* Builds aggregated vault by coin type - vending cache and user's money */
-    getAggregatedMoneyStore() {
-        let aggregatedStore = {};
-        Object.keys(this.moneyStore).forEach(key => {
-            if (key !== 'total') {
-                aggregatedStore[key] = this.moneyStore[key].qty + this.paidMoney[key];
-            }
-        });
-        aggregatedStore.total = this.moneyStore.total + this.paidMoney.total;
-        return aggregatedStore;
+        console.log(this._productsStore);
     }
 
     /* Builds array of all coins for the change or empty array */
-    calculateChange(diff, aggregatedMoneyStore) {
+    calculateChange(diff) {
         let change = [];
         let totalDiffValue = diff;
-        this.nominalMapping.reverse().forEach(nominalObj => {
+        this._nominalMapping.reverse().forEach(nominalObj => {
             if (totalDiffValue >= nominalObj.value) {
                 let coinsCount = Math.floor(totalDiffValue / nominalObj.value);
-                coinsCount = Math.min(coinsCount, aggregatedMoneyStore[nominalObj.code]);
+                let aggregatedCoinCount = this._moneyStore[nominalObj.code].qty + this._paidMoney[nominalObj.code];
+                coinsCount = Math.min(coinsCount, aggregatedCoinCount);
                 if (coinsCount > 0) {
                     totalDiffValue = totalDiffValue - (coinsCount * nominalObj.value);
-                    change.push({code: nominalObj.code, qty: coinsCount});
+                    let strCoinIds = (nominalObj.code + ',').repeat(coinsCount);
+                    change = change.concat(strCoinIds.substring(0, strCoinIds.length-1).split(','));
                 }
             }
         });
@@ -84,18 +67,21 @@ class VendingMachine {
     }
 
     /* Decrements number of coins due to change return and re-calculates total*/
-    updateMoneyInventory(changeObj, aggregatedMoneyStore) {
+    updateMoneyInventory(changeObj) {
+        /* Decrement amount of returned coins */
         changeObj.forEach(element => {
-            aggregatedMoneyStore[element.code] = aggregatedMoneyStore[element.code] - element.qty;
+            this._moneyStore[element].qty = this._moneyStore[element].qty + this._paidMoney[element] - 1;
         });
-        Object.keys(aggregatedMoneyStore).forEach(key => {
-            if (key !== 'total') {
-                this.moneyStore[key].qty = aggregatedMoneyStore[key];
+        /* Increment amount of coins received by user */
+        Object.keys(this._moneyStore).forEach(key => {
+            if (key !== 'total' && changeObj.indexOf(key) < 0) {
+                this._moneyStore[key].qty = this._moneyStore[key].qty + this._paidMoney[key];
             }
-        });
-        this.moneyStore.total = Object.keys(this.moneyStore).reduce((total, key) => {
-            if (this.moneyStore[key].hasOwnProperty('value')) {
-                return total +  (this.moneyStore[key].qty * this.moneyStore[key].value);
+        })
+        /* Calculates total available money in store */
+        this._moneyStore.total = Object.keys(this._moneyStore).reduce((total, key) => {
+            if (this._moneyStore[key].hasOwnProperty('value')) {
+                return total +  (this._moneyStore[key].qty * this._moneyStore[key].value);
             } else {
                 return total;
             }
@@ -104,20 +90,20 @@ class VendingMachine {
 
     /* Decrements product stock level */
     updateProductInventory(slotId) {
-        this.productsStore[slotId].qty -= 1;
+        this._productsStore[slotId].qty -= 1;
     }
 
     /* Processes user's selection, generates change, updates inventory */
     processOrder(slotId, product) {
-        let aggregatedMoneyStore = this.getAggregatedMoneyStore();
+        //let aggregatedMoneyStore = this.getAggregatedMoneyStore();
         let changeObj = [];
-        let diff = (this.paidMoney.total - product.price) * 100;
+        let diff = (this._paidMoney.total - product.price) * 100;
         /* Cannot return change less than 5c */
-        if (diff >= this.nominalMapping[0].value) {
-            changeObj = this.calculateChange(diff, aggregatedMoneyStore);
+        if (diff >= this._nominalMapping[0].value) {
+            changeObj = this.calculateChange(diff);
         }
         if (changeObj.length > 0) {
-            this.updateMoneyInventory(changeObj, aggregatedMoneyStore);
+            this.updateMoneyInventory(changeObj);
             this.updateProductInventory(slotId);
             this.resetUserPayment();
             console.log('Enjoy!');
@@ -134,16 +120,16 @@ class VendingMachine {
         let choice;
         do {
             choice = (await askForSelection()).toLowerCase();
-            if (this.moneyStore[choice] != null) {
+            if (this._moneyStore[choice] != null) {
                 this.updateTotalPaidAmound(choice);
-                console.log(`Tendered = ${this.paidMoney.total.toFixed(2)}\n`);
+                console.log(`Tendered = ${this._paidMoney.total.toFixed(2)}\n`);
             }
-            if (this.productsStore[choice] != null) {
-                let productObj = this.productsStore[choice];
+            if (this._productsStore[choice] != null) {
+                let productObj = this._productsStore[choice];
                 if (productObj.qty < 1) {
                     console.log('This product is not available. Please try again.')
                     this.resetUserPayment();
-                } else if (productObj.price > this.paidMoney.total) {
+                } else if (productObj.price > this._paidMoney.total) {
                     console.log('The amount paid is not enough. Please try again.');
                 } else if (!this.processOrder(choice, productObj)) {
                     console.log('Not enough availability. Cannot return change.');
@@ -159,8 +145,8 @@ class VendingMachine {
     displayWelcomeMsg() {
         console.log('Welcome to the Vending Machine simulator!\n');
 		console.log('The vending machine contains the following products\n');
-        Object.keys(this.productsStore).forEach((key) => {
-            let slotObj = this.productsStore[key];
+        Object.keys(this._productsStore).forEach((key) => {
+            let slotObj = this._productsStore[key];
             var slotId = key.replace(/^\w/, c => c.toUpperCase());
             console.log(`${slotId} - ${slotObj.qty} x ${slotObj.name} = ${slotObj.price.toFixed(2)}\n`);
         });
