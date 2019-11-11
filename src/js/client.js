48296473e4001f78323bf9a94f937683eea0eae1
Daniel Nikolov <dnikolov@gmail.com>
2019-11-11 23:13:35 +0200

    Update frontend

--- src/js/client.js
+++ src/js/client.js
@@ -6,10 +6,15 @@ window.vendingMachine = new VendingMachine(config.products, config.money, config
 function loadProducts() {
     Object.keys(window.vendingMachine.productsStore).forEach(key => {
         let productObj = window.vendingMachine.productsStore[key];
-        let productDivStr = '<div class="product d-flex col-lg-3 col-sm-4 col-6">' +
-            '<span class="product__name row col-12 justify-content-center align-items-center">' +
-            productObj.name + '</span>' +
-            '<span class="product__qty">' + productObj.qty + '</span></div>';
+        let productDivStr =
+            '<div class="product d-flex col-lg-3 col-sm-4 col-6 justify-content-center align-items-center">' +
+                '<div class="details">' +
+                    '<span class="product__name col-12 d-flex justify-content-center">' +
+                    productObj.name +
+                    '</span>' +
+                    '<span class="product__qty col-12 d-flex justify-content-center">Qty: ' + productObj.qty + '</span>' +
+                '</div>' +
+            '</div>';
         $('.products').append($(productDivStr));
     });
 }
