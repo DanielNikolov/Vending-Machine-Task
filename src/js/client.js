07d174343b772550983b649198e42684d75cfbb5
DanielNikolov <dnikolov@gmail.com>
2019-11-12 18:14:01 +0200

    Update FE functionality

--- src/js/client.js
+++ src/js/client.js
@@ -7,21 +7,18 @@ function loadProducts() {
     $('.products').empty();
     Object.keys(window.vendingMachine.productsStore).forEach(key => {
         let productObj = window.vendingMachine.productsStore[key];
-        let productDivStr =
-            '<div class="product d-flex col-lg-3 col-sm-4 col-6 justify-content-center align-items-center" data-slotid="' + key + '">' +
-                '<div class="details">' +
-                    '<span class="product__name col-12 d-flex justify-content-center">' +
-                        '<strong>' + productObj.name + '</strong>' +
-                    '</span>' +
-                    '<span class="product__qty col-12 d-flex justify-content-center">Qty: ' + productObj.qty + '</span>' +
-                    '<span class="product__price col-12 d-flex justify-content-center">$' + productObj.price.toFixed(2) + '</span>' +
-                '</div>' +
-            '</div>';
-        let $productDiv = $(productDivStr);
+        let $product = $('<div class="product d-flex col-lg-3 col-sm-4 col-6 justify-content-center align-items-center" data-slotid="' + key + '"></div>');
+        let $productDetails = $('<div class="details"></div>');
+        let $productName = $('<span class="product__name col-12 d-flex justify-content-center"></span>');
+        $productName.html('<strong>' + productObj.name + '</strong>');
+        let $productQty = $('<span class="product__qty col-12 d-flex justify-content-center">Qty: ' + productObj.qty + '</span>');
+        let $productPrice = $('<span class="product__price col-12 d-flex justify-content-center">$' + productObj.price.toFixed(2) + '</span>');
+        $productDetails.append($productName, $productQty, $productPrice);
+        $product.append($productDetails);
         if (productObj.qty < 1) {
-            $productDiv.addClass('product--disabled');
+            $product.addClass('product--disabled');
         }
-        $('.products').append($productDiv);
+        $('.products').append($product);
     });
 }
 
@@ -35,17 +32,14 @@ function loadMoneyOptions() {
         return 0;
     });
     sortedNominalMapping.forEach(element => {
-        let coinButtonStr =
-            '<div class="coin d-flex col-lg-3 col-sm-4 col-6 justify-content-center align-items-center" data-coinid="' + element[0] + '">' +
-                '<span><strong>' + element[0] + '</strong></span>' +
-            '</div>';
-        $('.moneypad').append($(coinButtonStr))
+        let $coinButton = $('<div class="coin d-flex col-lg-3 col-sm-4 col-6 justify-content-center align-items-center" data-coinid="' + element[0] + '"></div>');
+        $coinButton.append('<span><strong>' + element[0] + '</strong></span>');
+        $('.moneypad').append($coinButton);
     });
-    let resetButtonStr =
-        '<div class="reset coin d-flex col-lg-3 col-sm-4 col-6 justify-content-center align-items-center">' +
-            '<span><strong>Reset</strong></span>' +
-        '</div>';
-    $('.moneypad').append($(resetButtonStr));
+
+    let $resetButton = $('<div class="reset coin d-flex col-lg-3 col-sm-4 col-6 justify-content-center align-items-center"></div>');
+    $resetButton.append('<span><strong>Reset</strong></span>');
+    $('.moneypad').append($resetButton);
 }
 
 function initProductEvents() {
