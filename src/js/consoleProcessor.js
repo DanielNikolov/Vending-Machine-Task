49d6b92de85be38a5acde0e989756604bb2c6e49
DanielNikolov <dnikolov@gmail.com>
2019-11-13 16:46:41 +0200

    Update project setup

--- src/js/consoleProcessor.js
+++ src/js/consoleProcessor.js
@@ -15,10 +15,6 @@ class ConsoleProcessor {
         this._messages = new Messages();
     }
 
-    askForSelection() {
-        return new Promise(resolve => readLine.question(`${this._messages.enterSelectionMessage}`, response => resolve(response)));
-    }
-
     displayWelcomeMessage() {
         console.log(this._messages.titleMessage);
         console.log(this._messages.headerMessage);
@@ -27,15 +23,16 @@ class ConsoleProcessor {
             var slotId = key.replace(/^\w/, c => c.toUpperCase());
             console.log(`${slotId} - ${slotObj.qty} x ${slotObj.name} = ${slotObj.price.toFixed(2)}\n`);
         })
-        console.log(this._messages.coinsMessage);
-        console.log(this._messages.actionsCoinMessage);
-        console.log(this._messages.actionsSlotMessage);
+        console.log([this._messages.coinsMessage, this._messages.actionsCoinMessage, this._messages.actionsSlotMessage].join('\n'));
     }
 
     async getUserInput() {
         let choice;
         do {
-            choice = (await this.askForSelection()).toLowerCase();
+            let askForSelection = () => {
+                return new Promise(resolve => readLine.question(`${this._messages.enterSelectionMessage}`, response => resolve(response)));
+            }
+            choice = (await askForSelection()).toLowerCase();
             let product = this._vendingMachine.getSlot(choice);
             if (this._vendingMachine.isCoin(choice)) {
                 console.log(`${this._messages.tenderedMessage} ${this._vendingMachine.addUserCoin(choice).toFixed(2)}\n`);
@@ -47,11 +44,11 @@ class ConsoleProcessor {
                 } else {
                     let result = this._vendingMachine.processSlotSelection(choice);
                     if (result.length > 0) {
-                        console.log(`${this._messages.enjoyMessage}`);
+                        console.log(this._messages.enjoyMessage);
                         console.log(`${this._messages.itemMessage} ${product.name}`);
                         console.log(`${this._messages.changeMessage} ${result.join(',')}\n`);
                     } else {
-                        console.log(`${this._messages.noChangeMessage}`);
+                        console.log(this._messages.noChangeMessage);
                     }
                 }
             } else if (choice === 'inventory') {
