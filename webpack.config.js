988fedc07839f5f29c1a8d3c04baf67dcd735d39
Daniel Nikolov <dnikolov@gmail.com>
2019-11-14 23:33:13 +0200

    Fix project setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,44 @@
 const path = require("path");
-const glob = require('glob');
 
-function getEntries(patterns) {
-    const entries = {};
-    patterns.forEach((pattern) => {
-        glob.sync(pattern).forEach((file) => {
-            entries[file.replace('src/', '')] = path.join(__dirname, file);
-        });
-    });
-    console.log(entries);
-    return entries;
-}
-
-module.exports = [
-    {
-        mode: 'development',
-        entry: './src/js/client.js',
-        name: 'js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'js/client.js',
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js?/,
-                    exclude: [
-                        '/test',
-                        '/node_modules/'
-                    ],
-                    use: ["babel-loader"]
-                }
-            ]
-        },
-        resolve: {
-            extensions: ['.js']
-        }
+module.exports = {
+    entry: ["./src/js/client.js", "./src/scss/client.scss"],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "js/client.js"
     },
-    {
-        mode: 'production',
-        entry: './src/scss/client.scss',
-        name: 'scss',
-        module: {
-            rules: [
-                {
-                    test: /\.scss?/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: 'css/[name].css',
-                            }
-                        },
-                        {
-                            loader: 'extract-loader'
-                        },
-                        {
-                            loader: 'css-loader?-url'
-                        },
-                        {
-                            loader: 'sass-loader'
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'css/[name].css'
                         }
-                    ]
-                }
-            ]
-        },
-        resolve: {
-            extensions: ['.scss']
-        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader?-url'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.js?/,
+                exclude: [
+                    "/test/",
+                    "/node_modules/"
+                ],
+                use: ["babel-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".scss"]
     }
-]
+}
